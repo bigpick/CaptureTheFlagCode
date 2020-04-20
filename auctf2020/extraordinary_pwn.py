0a3f12c6b75668962b25b1d1395b63b7e40653d9
George Pickering <9803299+bigpick@users.noreply.github.com>
2020-04-20 11:12:23 -0400

    more ctfs

---
+++ auctf2020/extraordinary_pwn.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from pwn import *
+
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ#{}_-+[,.=!@$%&0123456789 "
+
+# Initially, we know starts with F#{
+key_so_far="auctf{"
+
+while 1<2:
+    conn = remote('challenges.auctf.com', 30030)
+    i = 0
+    for _ in range(100):
+        conn.sendline(key_so_far+ALPHABET[i])
+        response = conn.recvline().split()[-1].decode("utf-8")
+        size = len(key_so_far+ALPHABET[i])
+        if response == ("b''"):
+            # Found next char
+            key_so_far+=ALPHABET[i]
+            print(key_so_far)
+            i=0
+            continue
+        # Try lower case
+        if ALPHABET[i].isalpha():
+            conn.sendline(key_so_far+(ALPHABET[i].lower()))
+            response = conn.recvline().split()[-1].decode("utf-8")
+            if response == ("b''"):
+                # Found next char
+                key_so_far+=ALPHABET[i].lower()
+                print(key_so_far)
+                i=0
+        i+=1
